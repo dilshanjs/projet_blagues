7dcc971e596d5ee98897ce9bdeb59eb481d7b10d
Alexa <alexa.duval@esme.fr>
2025-03-11 10:04:49 +0100

    Ajout du choix de catégorie de blague

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pyjokes
 
-def tell_joke(lang="en"):
-    print(pyjokes.get_joke(language=lang))
+def tell_joke(lang="en", category="all"):
+    print(pyjokes.get_joke(language=lang,category=category))
 
 if __name__ == "__main__":
     lang = input ("Choisissez une langue (en, de, es, it, gl, ) : ")
-    tell_joke(lang)
+    joke_type = input ("Choisissez une categorie de blague (neutral, chuck, all ):")                                                                                 
+    tell_joke(lang, joke_type)
 
 
 
